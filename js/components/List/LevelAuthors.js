6fee0987e6a9cdacb5d38c14ff9d376415d2f516
2mbbl <suynguyetdinh@gmail.com>
2026-08-07 19:35:50 +0700

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -39,12 +39,12 @@ export default {
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm"></div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
+            <div class="type-title-sm"></div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
