aab0330d176b0ac4b7f4db49a67e16a1fd8a7e56
2mbbl <suynguyetdinh@gmail.com>
2026-08-09 00:53:35 +0700

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -26,7 +26,7 @@ export default {
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">Verifier/First victor</div>
+                <div class="type-title-sm">First victor</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
